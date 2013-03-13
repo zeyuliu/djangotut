5bff2e500bcec43d73dbe8cb56432fddf7624361
Andrew <zeyuliu@gmail.com>
2013-03-12 17:51:04 -0700

    added configs for static content

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from polls.models import Poll
 from django.template import Context, loader
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def index(request):
-	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-	output = ', '.join([p.question for p in latest_poll_list])
-	return HttpResponse(output)
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    return render_to_response('polls/index.html', context_instance=RequestContext(request))
 
 def detail(request, poll_id):
 	return HttpResponse("You're looking at poll %s." % poll_id)
