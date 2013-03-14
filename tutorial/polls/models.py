b6c237f1ceadf79b4cb26611a7e7bd47c518896c
Andrew <zeyuliu@gmail.com>
2013-03-13 21:51:57 -0700

    detail view

--- tutorial/polls/models.py
+++ tutorial/polls/models.py
@@ -24,8 +24,8 @@ class Poll(models.Model):
 
 class Choice(models.Model):
 	poll = models.ForeignKey(Poll)
-	choice_text = models.CharField(max_length=200)
+	choice = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
 
 	def __unicode__(self):
-		return self.choice_text
+		return self.choice
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
 	url(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
     url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
     url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+    url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
     # Examples:
     # url(r'^$', 'tutorial.views.home', name='home'),
     # url(r'^tutorial/', include('tutorial.foo.urls')),
