437ca75506f1c4bcc6cd01bf5801b1466a6a11c8
Andrew <zeyuliu@gmail.com>
2013-03-14 15:16:09 -0700

    working results page

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from polls.models import Poll
-from django.template import Context, loader
+from django.template import Context, loader, RequestContext
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.shortcuts import render_to_response, get_object_or_404
-from django.template import RequestContext
+from django.core.urlresolvers import reverse
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
@@ -15,7 +15,8 @@ def detail(request, poll_id):
     return render_to_response('polls/detail.html', {'poll': p}, context_instance=RequestContext(request))
 
 def results(request, poll_id):
-	return HttpResponse("You're looking at the results of poll %s." %poll_id)
+	p = get_object_or_404(Poll, pk=poll_id)
+	return render_to_response('polls/results.html', {'poll': p})
 
 def vote(request, poll_id):
 	p = get_object_or_404(Poll, pk=poll_id)
@@ -31,4 +32,3 @@ def vote(request, poll_id):
 		selected_choice.votes+=1
 		selected_choice.save()
 		return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
-
